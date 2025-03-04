d0300b567338fd0d20418594d3550aa53104fd0e
Tina Wulff <tina@tinawulff.dk>
2025-03-04 08:57:27 +0100

    mere fetch detaljer

--- js/frontpage.js
+++ js/frontpage.js
@@ -48,12 +48,14 @@ sectionNowShowing.classList.add("nowShowing")
 main.append(sectionNowShowing);
 
 let headlineNow = document.createElement("h2");
-headlineNow.innerHTML = "Now Showing";
 
+headlineNow.innerHTML = "Now Showing";
+headlineNow.classList.add("nowShowing__headline")
 sectionNowShowing.append(headlineNow);
 
+
 function fetchMovie() { 
-fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}'https://api.themoviedb.org/3/trending/movie/week", {
+fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc", {
     headers: {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDYyY2I2YzRlYmRhMGY1MDA2ZDYyNDY4N2IxMjhkYiIsIm5iZiI6MTc0MDk4Njc3Ny4xNjEsInN1YiI6IjY3YzU1OTk5ODgxYzAxM2VkZTdhNmZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6OZhglLj6yu_2o1SQDcbLtUloW0Z0qhIE7r1-ZmZeN8'
@@ -75,9 +77,9 @@ fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_v
 sectionNowShowing.innerHTML += data.results.map((movie) => {
     return`
 
-    <a href="detail-movie.html">
-    <article>
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+    <a class="now-showing__linkCard" href="detail-movie.html">
+    <article class="now-showing__card">
+        <img loading="lazy" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
         <h3>${movie.title}</h3>
         <p><i class="fa-solid fa-star"></i>${movie.vote_average}/10 IMDb</p>
     </article>
@@ -86,10 +88,89 @@ sectionNowShowing.innerHTML += data.results.map((movie) => {
 }).join("");
 
 
-
 })
-
-
-
 }
+
+// SHOWING NOW SLUT
+
+// POPULAR SECTION BEGYND
+
+let sectionPopular = document.createElement("section");
+sectionPopular.classList.add("sectionPopular");
+main.append(sectionPopular);
+
+let headlinePopular = document.createElement("h2");
+headlinePopular.classList.add("sectionPopular__headline");
+headlinePopular.innerHTML = "Popular"
+sectionPopular.append(headlinePopular);
+
+
+function fetchPopular() { 
+    fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc", {
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDYyY2I2YzRlYmRhMGY1MDA2ZDYyNDY4N2IxMjhkYiIsIm5iZiI6MTc0MDk4Njc3Ny4xNjEsInN1YiI6IjY3YzU1OTk5ODgxYzAxM2VkZTdhNmZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6OZhglLj6yu_2o1SQDcbLtUloW0Z0qhIE7r1-ZmZeN8'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("movie findes ikke!!!!");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Populære film:", data.results);
+
+        // Fetch detaljer for hver film
+        let movieDetailsFetches = data.results.map(movie => {
+            return fetch(`https://api.themoviedb.org/3/movie/${movie.id}?append_to_response=genres,runtime`, {
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDYyY2I2YzRlYmRhMGY1MDA2ZDYyNDY4N2IxMjhkYiIsIm5iZiI6MTc0MDk4Njc3Ny4xNjEsInN1YiI6IjY3YzU1OTk5ODgxYzAxM2VkZTdhNmZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6OZhglLj6yu_2o1SQDcbLtUloW0Z0qhIE7r1-ZmZeN8'
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Fejl ved hentning af film detaljer!");
+                }
+                return response.json();
+            });
+        });
+
+        // Når alle fetches er færdige, kombinér dataene
+        return Promise.all(movieDetailsFetches).then((moviesDetails) => {
+            let combinedMovies = data.results.map((movie, index) => ({
+                ...movie, // Beholder originaldata
+                genres: moviesDetails[index].genres, // Tilføjer genres
+                runtime: moviesDetails[index].runtime // Tilføjer runtime
+            }));
+
+            return combinedMovies;
+        });
+    })
+    .then((movieWithDetails) => {
+        console.log("Film med ekstra detaljer: ", movieWithDetails);
+
+        sectionPopular.innerHTML += movieWithDetails.map((movie) => {
+            return `
+                <a class="now-showing__linkCard" href="detail-movie.html">
+                <article class="now-showing__card">
+                    <img loading="lazy" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <h3>${movie.title}</h3>
+                    <p><i class="fa-solid fa-star"></i>${movie.vote_average}/10 IMDb</p>
+                    <div class="genre-container">
+                        ${movie.genres.map(genre => `<p class="genre">${genre.name}</p>`).join("")}
+                    </div>
+                    <p class="time"><i class="fa-regular fa-clock"></i> ${movie.runtime} min</p>
+                </article>
+                </a>    
+            `;
+        }).join("");
+    })
+    .catch(error => {
+        console.error("Fejl:", error);
+    });
+}
+
+fetchPopular();
 fetchMovie();
